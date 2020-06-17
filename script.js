e6699a216feb8910b36d2daff3581c3ac51822c4
Matthew Huus <matthew.huus3@gmail.com>
2020-06-16 22:13:21 -0500

    working questions pop up and input bar on high score page.

--- script.js
+++ script.js
@@ -12,7 +12,8 @@
 
 // VARIABLES
 var score = 0;
-var currentQuestion = 0;
+var currentQuestionIndex = 0;
+
 var question = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
 var answers = [
     ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
@@ -25,27 +26,34 @@ var correctAnswer = [1, 3, 2, 0, 1];
 //referencing the specific answers:
 //answers[3][3] = Answer 4 of Question 4
 var timeRemaining = document.getElementById("time-remaining");
-
+var displayedQuestionNum = document.getElementById("question-number")
 
 // Functions
 // countdown timer on click start
 
 
 function startGame() {
-
-
-countdownTimer();
-    
+    countdownTimer();
+    countQuestionNumber();
 }
-function answersAppear(){
-    // have Start button disappear
-    
+
+function answersAppear() {
+    var startButton = document.getElementById("start");
+    var answers = document.getElementById("answers")
+    if (answers.style.display === "none") {
+
+        startButton.style.display = "block";
+        answers.style.display = "none";
+    } else {
+        answers.style.display = "block";
+        startButton.style.display = "none"
+    }
 }
 function countdownTimer() {
     var timeLeft = 60;
     var timeInterval = setInterval(function () {
         timeRemaining.textContent = timeLeft;
-        
+
 
         if (timeLeft === 0) {
             timeRemaining.textContent = "";
@@ -57,31 +65,28 @@ function countdownTimer() {
 
 
 }
-// function toggleScreen(){
-//     var mainMenu = document.getElementById("start");
-//     var inGame = document.getElementById("question-number");
+// for any answer clicked run this function (COME BACK TO THIS)
+function countQuestionNumber(){
+    var firstQuestion = 1;
+    displayedQuestionNum.textContent = firstQuestion;
 
-//     var mainMenuToggle = mainMenu.style.display;
-//     var inGameToggle = inGame.style.display;
+    if(firstQuestion>10){
+        // endGame function
+    }
+    firstQuestion = firstQuestion+1;
+}
 
-//     if(mainMenuToggle=='block' && startGame()){
-//         inGameToggle = 'block';
-//         mainMenuToggle = 'none';
-//     }else mainMenuToggle = 'block';
-//         inGameToggle = 'none';
-
-// }
-function displayCurrentQuestion(){
-var questionNumber = question[currentQuestion];
-var displayQuestionHere = document.getElementById("text-area");
-displayQuestionHere.textContent = questionNumber.title;
+function displayCurrentQuestion() {
+    var questionNumber = question[currentQuestionIndex];
+    var displayQuestionHere = document.getElementById("text-area");
+    displayQuestionHere.textContent = questionNumber.title;
 
 
 }
-// ?? First, question needs to go to next question after they answer question. (repeat until time===0 || question[i]<question[10])
+    // ?? First, question needs to go to next question after they answer question. (repeat until time===0 || question[i]<question[10])
     // check if correct asnwer === their answer
-        // yes, next question add 1 to score
-        // else, -5 seconds and next question
+    // yes, next question add 1 to score
+    // else, -5 seconds and next question
 
 
 
@@ -89,8 +94,8 @@ displayQuestionHere.textContent = questionNumber.title;
 
 
 
-// function endGame(){}
-// this function will lead to an opening of the top-scores.html file and displaying the scores from high to low using local storage
+    // function endGame(){}
+    // this function will lead to an opening of the top-scores.html file and displaying the scores from high to low using local storage
 
 
 
@@ -98,6 +103,4 @@ displayQuestionHere.textContent = questionNumber.title;
 // EVENT LISTENERS
 //  on start click start the countdown timer
 // on key pressed display their initials
-// on click answer compare their answer to the correct answer
-
-
+// on click answer compare their answer to the correct
